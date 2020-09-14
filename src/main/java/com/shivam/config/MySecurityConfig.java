bbfffbb2cfaa0afb2b9c34764af4457f2f51b073
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-14 20:29:55 +0530

    Custom login page for security

--- src/main/java/com/shivam/config/MySecurityConfig.java
+++ src/main/java/com/shivam/config/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.shivam.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,5 +22,19 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 									 .withUser(users.username("admin1").password("s").roles("ADMIN"));
 		
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests() 						// Allow restriction based on HttpServletRequests
+		.anyRequest().authenticated() 					// For any request coming in, authenticate
+		.and()											// AND
+		.formLogin()     								// Customise the login form
+		.loginPage("/myLoginPage")						// Use this login page
+		.loginProcessingUrl("/authenticateDetails")		// Process the login details at this URL
+		.permitAll();									// Allow all to see login page
+	}
+	
+	
 
 }
