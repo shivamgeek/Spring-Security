b158c6090b00f5270b6f4949d654abbf6f044751
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-14 17:32:56 +0530

    Added Spring Security filter/initializer

---
+++ src/main/java/com/shivam/config/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.shivam.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
