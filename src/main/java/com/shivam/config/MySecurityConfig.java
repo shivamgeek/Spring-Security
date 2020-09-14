733f05c9b305ebec36406f99909b6445f8a01dbc
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-14 23:02:58 +0530

    Show user name and role on home, using thymeleaf-extras-security

--- src/main/java/com/shivam/config/MySecurityConfig.java
+++ src/main/java/com/shivam/config/MySecurityConfig.java
@@ -18,7 +18,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		
 		auth.inMemoryAuthentication().withUser(users.username("employee1").password("s").roles("EMPLOYEE"))
-									 .withUser(users.username("manager1").password("s").roles("MANAGER"))
+									 .withUser(users.username("manager1").password("s").roles("MANAGER", "EMPLOYEE"))
 									 .withUser(users.username("admin1").password("s").roles("ADMIN"));
 		
 	}
@@ -27,7 +27,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests() 						// Allow restriction based on HttpServletRequests
-		.anyRequest().authenticated() 					// For any request coming in, authenticate
+		.anyRequest().authenticated()					// For any request coming in, authenticate
 		.and()											// AND
 		.formLogin()     								// Customize the login form
 		.loginPage("/myLoginPage")						// Use this login page
