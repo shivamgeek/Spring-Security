96dad80ae2bc1390bda01f2c61c9f108301dee2c
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-14 17:55:07 +0530

    Added Spring security config

---
+++ src/main/java/com/shivam/config/MySecurityConfig.java
@@ -0,0 +1,25 @@
+package com.shivam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication().withUser(users.username("employee1").password("s").roles("EMPLOYEE"))
+									 .withUser(users.username("manager1").password("s").roles("MANAGER"))
+									 .withUser(users.username("admin1").password("s").roles("ADMIN"));
+		
+	}
+
+}
